8c9ce3a01c68d86160a75cf1d7dd1d0967c51db9
rafay11322-sketch <rafay.11322@theacademy.edu.pk>
2026-06-27 16:57:34 +0500

    Add Firebase configuration to auth.js

---
+++ auth.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSy...............",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789:web:abcdef123456"
+};
