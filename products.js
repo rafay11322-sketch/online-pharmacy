2e9d4972c530a37749f2f43fa0c7afde8648cea4
rafay11322-sketch <rafay.11322@theacademy.edu.pk>
2026-06-27 16:56:01 +0500

    Create products.js

---
+++ products.js
@@ -0,0 +1,83 @@
+const products = [
+
+{
+id:1,
+name:"Panadol Extra",
+price:350,
+category:"Pain Relief",
+image:"images/panadol.jpg"
+},
+
+{
+id:2,
+name:"Augmentin 625mg",
+price:890,
+category:"Antibiotics",
+image:"images/augmentin.jpg"
+},
+
+{
+id:3,
+name:"Disprin",
+price:120,
+category:"Pain Relief",
+image:"images/disprin.jpg"
+},
+
+{
+id:4,
+name:"Brufen 400mg",
+price:210,
+category:"Pain Relief",
+image:"images/brufen.jpg"
+},
+
+{
+id:5,
+name:"Centrum Multivitamin",
+price:2100,
+category:"Vitamins",
+image:"images/centrum.jpg"
+},
+
+{
+id:6,
+name:"Vitamin C Tablets",
+price:680,
+category:"Vitamins",
+image:"images/vitaminc.jpg"
+},
+
+{
+id:7,
+name:"Blood Pressure Monitor",
+price:4999,
+category:"Medical Devices",
+image:"images/bpmonitor.jpg"
+},
+
+{
+id:8,
+name:"Digital Thermometer",
+price:850,
+category:"Medical Devices",
+image:"images/thermometer.jpg"
+},
+
+{
+id:9,
+name:"Face Mask Pack",
+price:450,
+category:"Healthcare",
+image:"images/mask.jpg"
+},
+
+{
+id:10,
+name:"Hand Sanitizer",
+price:320,
+category:"Healthcare",
+image:"images/sanitizer.jpg"
+}
+
+];
